16cc3dc0282b421101a318959ee0a3329858c20c
usmanarif460 <usmanarif460@gmail.com>
2022-07-06 15:39:46 +0500

    made changes in dark mode

--- js/scripts.js
+++ js/scripts.js
@@ -32,9 +32,102 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-
+// "assets/mobile-dark.svg"
 const chk = document.getElementById('chk');
+const bgLight = document.querySelectorAll('.bg-light')
+const inputs = document.querySelectorAll('.bg-light');
+const blueButton = document.getElementById('get-started');
+const darkText = document.querySelectorAll('.text-dark');
+const realEstate = document.getElementById('real-estate');
+const mobileSvg = document.getElementById('mobile');
 
+console.log(darkText);
 chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
+    if (mobileSvg.hasAttribute('src', "assets/mobile.svg")) {
+        mobileSvg.setAttribute('src', "assets/mobile-dark.svg")
+       
+    }
+    else if (mobileSvg.hasAttribute('src', "assets/mobile-dark.svg")) {
+        mobileSvg.setAttribute('src', "assets/mobile.svg")
+        
+    }
+   
+    const masthead = document.getElementById('masthead')
+    if (masthead.classList.contains('bg-white')) {
+        masthead.classList.remove('bg-white');
+        masthead.classList.add("bg-dark")
+        masthead.classList.add('text-white')
+    }
+    else if(masthead.classList.contains('bg-dark')){
+        masthead.classList.remove('bg-dark');
+        masthead.classList.add('bg-white')
+        masthead.classList.remove('text-white')
+    }
+    bgLight.forEach(el => {
+        if (el.classList.contains('bg-light') ) {
+            el.classList.remove('bg-light');
+            el.classList.add("bg-dark")
+            el.classList.add('text-white')
+        }
+        else if (el.classList.contains('bg-gray-50') ) {
+            el.classList.add('bg-light');
+            el.classList.remove("bg-gray-50")
+            el.classList.remove('text-white')
+        }
+    })
+    if (blueButton.classList.contains('bg-primary')){
+        blueButton.classList.remove('bg-primary');
+        blueButton.classList.add('bg-gray-100');
+    }
+    else if (blueButton.classList.contains('bg-gray-100')) {
+        blueButton.classList.add('bg-primary');
+        blueButton.classList.remove('bg-gray-100');
+
+    }
+    darkText.forEach(el => {
+        if (el.classList.contains('text-dark') ) {
+            el.classList.remove('text-dark');
+            el.classList.add('text-white')
+        }
+        else if (el.classList.contains('text-white') ) {
+            el.classList.remove('text-white');
+            
+            el.classList.add('text-dark')
+        }
+    })
+    inputs.forEach(el => {
+        if (el.classList.contains('form-control') ) {
+            el.classList.add('bg-black');
+            el.classList.remove('form-control')
+        }
+        else if (el.classList.contains('bg-black') ) {
+            el.classList.remove('bg-black');
+            
+            el.classList.add('form-control')
+        }
+    })
+    if (realEstate.classList.contains('real-estate')) {
+        realEstate.classList.remove('real-estate')
+        realEstate.classList.add('real-estate-dark')
+        realEstate.classList.add('text-white')
+        realEstate.classList.remove('text-dark')
+        
+    }
+    else if (realEstate.classList.contains('real-estate-dark')) {
+        realEstate.classList.add('text-dark');
+        realEstate.classList.remove('text-white')
+        realEstate.classList.remove('real-estate-dark')
+        realEstate.classList.add('real-estate')
+    }
+//    console.log(bgLight);
+    if (document.body.classList.contains('bg-white')) {
+        document.body.classList.add('bg-dark');
+        document.body.classList.remove('bg-white')
+    }
+    else if (document.body.classList.contains('bg-dark')) {
+        document.body.classList.add('bg-white');
+        document.body.classList.remove('bg-dark')
+    }
+    
+    
 });
